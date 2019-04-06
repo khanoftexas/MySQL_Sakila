ba8da3e0fdde21b169146acfd7e4395dd4c6ad30
khanoftexas <khanoftexas@gmail.com>
2019-04-06 09:59:43 -0500

    Initial Upload

--- sakila.sql
+++ sakila.sql
@@ -59,25 +59,64 @@ select * from actor;
 alter table actor
 drop column description;
 
-select * from actor;
+SELECT 
+    *
+FROM
+    actor;
 
 -- 4a. List the last names of actors, as well as how many actors have that last name.
-select last_name 'Last Name', Count(last_name) 'Actor Count' from actor
-group by last_name;
+SELECT 
+    last_name 'Last Name', COUNT(last_name) 'Actor Count'
+FROM
+    actor
+GROUP BY last_name;
 
 -- 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
-select last_name 'Last Name', Count(last_name) 'Actor Count' from actor
-group by last_name having count(last_name) >= 2;
+SELECT 
+    last_name 'Last Name', COUNT(last_name) 'Actor Count'
+FROM
+    actor
+GROUP BY last_name
+HAVING COUNT(last_name) >= 2;
 
 -- 4c. The actor HARPO WILLIAMS was accidentally entered in the actor table as GROUCHO WILLIAMS. Write a query to fix the record.
-select * from actor where first_name = 'GROUCHO' and last_name = 'WILLIAMS';
-update actor set first_name='HARPO' where first_name = 'GROUCHO' and last_name = 'WILLIAMS';
-select * from actor where first_name = 'HARPO' and last_name = 'WILLIAMS';
+SELECT 
+    *
+FROM
+    actor
+WHERE
+    first_name = 'GROUCHO'
+        AND last_name = 'WILLIAMS';
+        
+UPDATE actor 
+SET 
+    first_name = 'HARPO'
+WHERE
+    first_name = 'GROUCHO'
+        AND last_name = 'WILLIAMS';
+        
+SELECT 
+    *
+FROM
+    actor
+WHERE
+    first_name = 'HARPO'
+        AND last_name = 'WILLIAMS';
 
 
 -- 4d. Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that GROUCHO was the correct name after all! In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO.
-update actor set first_name='GROUCHO' where first_name = 'HARPO';
-select * from actor where first_name = 'GROUCHO';
+UPDATE actor 
+SET 
+    first_name = 'GROUCHO'
+WHERE
+    first_name = 'HARPO';
+    
+SELECT 
+    *
+FROM
+    actor
+WHERE
+    first_name = 'GROUCHO';
 
 -- 5a. You cannot locate the schema of the address table. Which query would you use to re-create it?
 -- Hint: https://dev.mysql.com/doc/refman/5.7/en/show-create-table.html
@@ -85,34 +124,72 @@ select * from actor where first_name = 'GROUCHO';
 SHOW CREATE TABLE address;
 
 -- 6a. Use JOIN to display the first and last names, as well as the address, of each staff member. Use the tables staff and address:
-select * from staff;
-select * from address;
+SELECT 
+    *
+FROM
+    staff;
+    
+SELECT 
+    *
+FROM
+    address;
 
-select s.first_name 'First Name', s.last_name 'Last Name', a.address 'Address'
-from staff s
-inner join address a 
-on s.address_id = a.address_id;
+SELECT 
+    s.first_name 'First Name',
+    s.last_name 'Last Name',
+    a.address 'Address'
+FROM
+    staff s
+        INNER JOIN
+    address a ON s.address_id = a.address_id;
 
 -- 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment.
+SELECT 
+    *
+FROM
+    staff;
+
+SELECT 
+    *
+FROM
+    payment;
+    
+  SELECT 
+    s.first_name 'First Name',
+    s.last_name 'Last Name',
+    SUM(p.amount)
+FROM
+    staff s
+        INNER JOIN
+    payment p ON s.staff_id = p.staff_id
+GROUP BY s.first_name , s.last_name
 
 -- 6c. List each film and the number of actors who are listed for that film. Use tables film_actor and film. Use inner join.
+
 -- 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
+
 -- 6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name:
 
-
-
-
-
 -- 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters K and Q have also soared in popularity. Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
+
 -- 7b. Use subqueries to display all actors who appear in the film Alone Trip.
+
 -- 7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. Use joins to retrieve this information.
+
 -- 7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as family films.
+
 -- 7e. Display the most frequently rented movies in descending order.
+
 -- 7f. Write a query to display how much business, in dollars, each store brought in.
+
 -- 7g. Write a query to display for each store its store ID, city, and country.
+
 -- 7h. List the top five genres in gross revenue in descending order. (Hint: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
+
 -- 8a. In your new role as an executive, you would like to have an easy way of viewing the Top five genres by gross revenue. Use the solution from the problem above to create a view. If you haven't solved 7h, you can substitute another query to create a view.
+
 -- 8b. How would you display the view that you created in 8a?
+
 -- 8c. You find that you no longer need the view top_five_genres. Write a query to delete it.
 
 
